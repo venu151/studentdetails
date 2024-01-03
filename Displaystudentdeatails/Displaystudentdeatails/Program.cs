f9d7fca6cdf77adff092f32a3191e1424140389f
Vadde <128579345+venu151@users.noreply.github.com>
2024-01-03 20:15:16 +0530

    Add files via upload

---
+++ Displaystudentdeatails/Displaystudentdeatails/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Displaystudentdeatails
+{
+    class Program
+    {
+        static void Main()
+        {
+            // File path
+            string filePath = @"C:\Users\vadde\source\repos\Displaystudentdeatails\Displaystudentdeatails\students.txt";
+
+            // Read all lines from the file
+            string[] lines = File.ReadAllLines(filePath);
+
+            Console.WriteLine("student data from a text file");
+
+
+            foreach (string i in lines)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
